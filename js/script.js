8da6250ee9547e56ff7290ed76052ad98bb5c133
Alex Seitov <seitov_ai@mail.ru>
2020-04-09 16:21:26 +0300

    Сверстал блок Testimonials

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// mobile menu
+
 const menuToggle = document.querySelector('.header__mobile-icon');
 const mobileNavContainer = document.querySelector('.header__menu');
 
@@ -5,3 +7,43 @@ menuToggle.onclick = function(){
     menuToggle.classList.toggle('header__mobile-icon_active');
     mobileNavContainer.classList.toggle('header__menu_active');
 }
+
+// slider =======================================================
+
+let slideIndex = 1,
+    slides = document.querySelectorAll('.slider__item'),
+    dotsWrap = document.querySelector('.testimonials__dots'),
+    dots = document.querySelectorAll('.dots__item');
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+    dots.forEach((item) => item.classList.remove('dots__item_active'));
+
+    slides[slideIndex - 1].style.display = 'flex';
+    dots[slideIndex - 1].classList.add('dots__item_active');
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+dotsWrap.addEventListener('click', function(event) {
+    for (let i = 0; i <= dots.length; i++) {
+        if (event.target.classList.contains('dots__item') && event.target == dots[i-1]) {
+            currentSlide(i);
+        }
+    }
+});
